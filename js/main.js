f84c23f61bd93b053b90b58175e64862c8575e66
Philg112 <phil.grujovski@gmail.com>
2017-04-08 15:06:26 +1000

    Cell toggle and player factory created

--- js/main.js
+++ js/main.js
@@ -1,4 +1,51 @@
 $(document).ready(function() {
+    // ------- GLOBAL VARIABLES ---------
     
 
+    // ---------------------------
+
+    // ------- OBJECTS -----------
+    var Player = {
+        playerFactory: function(name) {
+            var player = {};
+            player.name = name;
+            player.isTurn = false;
+            return player;
+        }
+    };
+
+    var Human = {
+        humanFactory: function(name) {
+            var human = Player.playerFactory(name); // Base call
+            return human;
+        }
+    }
+
+    var Ai = {
+        aiFactory: function(name) {
+            var ai = Player.playerFactory(name); // Base call
+            return ai;
+        }
+    }
+
+    var gameBoard = {
+        $gameSquare: $(".game-square"),
+
+        toggleSquare: function() {
+            var currentBg = $(this).css("background-color");
+            if(currentBg === "rgb(128, 128, 128)") {
+                $(this).css({
+                    backgroundColor: "white"
+                });
+            } else {
+                $(this).css({
+                    backgroundColor: "gray"
+                });
+            }
+        },
+
+
+    };
+    
+    gameBoard.$gameSquare.on("click", gameBoard.toggleSquare);
 }); 
