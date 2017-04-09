8d44f6498f91c629ddcc8002c78d68cbdf1042fa
Philg112 <phil.grujovski@gmail.com>
2017-04-09 11:01:10 +1000

    Added players

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-$(document).ready(function() {
+// $(document).ready(function() {
     // ------- GLOBAL VARIABLES ---------
     
 
@@ -19,33 +19,44 @@ $(document).ready(function() {
             var human = Player.playerFactory(name); // Base call
             return human;
         }
-    }
+    };
 
     var Ai = {
         aiFactory: function(name) {
             var ai = Player.playerFactory(name); // Base call
             return ai;
         }
-    }
+    };
 
     var gameBoard = {
         $gameSquare: $(".game-square"),
-
         toggleSquare: function() {
-            var currentBg = $(this).css("background-color");
-            if(currentBg === "rgb(128, 128, 128)") {
-                $(this).css({
-                    backgroundColor: "white"
-                });
-            } else {
-                $(this).css({
-                    backgroundColor: "gray"
-                });
-            }
+            $(this).css({
+                backgroundColor: "gray"
+            });
+        }
+    };
+
+    var main = { 
+        player1: function(name) {
+            var p1 = Human.humanFactory(name);
+            p1.isTurn = true;
+            return p1;
+        },
+        player2: function(name) {
+            var p2 = Human.humanFactory(name);
+            return p2;
         },
 
-
+        checkTurn: function() {
+            if(this.player1().isTurn === true){
+                console.log("Player 1 turn");
+            } else {
+                console.log("Player 2 turn");
+            }
+        }
     };
-    
+    console.log(playGame.player1("p1"));
+    console.log(playGame.player2("p2"));
     gameBoard.$gameSquare.on("click", gameBoard.toggleSquare);
-}); 
+// }); 
